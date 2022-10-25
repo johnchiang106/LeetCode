8e671d7ec46d26bb5f4322073913befbfe2d0b31
john <johnjiang0409@gmail.com>
2022-10-25 17:24:43 -0500

    2022/10/25

--- 696. Count Binary Substrings/696. Count Binary Substrings.cpp
+++ 696. Count Binary Substrings/696. Count Binary Substrings.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int countBinarySubstrings(string s) {
         int ans = 0, j = 0, n = s.size();
-        for(int i = n-1; i > 0; i -= max(1,j), j = 0)
+        for(int i = n-1; i > 0; i -= max(1,j), ans += j, j = 0)
             while(i-1-j >= 0 && i+j < n && 
                   s[i+j] == s[i] && s[i-1-j] != s[i+j])
-                ++ans, ++j;
+                ++j;
         return ans;
     }
 };
