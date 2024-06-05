e29731e65bbbbe334bb6f10737ee3bda249f174e
chenghan <johnjiang0409@gmail.com>
2024-06-05 00:55:46 -0500

    2024/6/4

---
+++ 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit fast solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int maxLength = 0;
+        int n = nums.size();
+        int j = 0;
+        deque<int> dqmin, dqmax;
+
+        for (int i = 0; i < n; ++i)
+        {
+            while (!dqmax.empty() && dqmax.back() < nums[i]) dqmax.pop_back();
+
+            while (!dqmin.empty() && dqmin.back() > nums[i]) dqmin.pop_back();
+
+            dqmin.push_back(nums[i]);
+            dqmax.push_back(nums[i]);
+
+            if (dqmax.front() - dqmin.front() <= limit) maxLength = max(maxLength, i - j + 1);
+            else 
+            {
+                if (nums[j] == dqmax.front()) dqmax.pop_front();
+                else if (nums[j] == dqmin.front()) dqmin.pop_front();
+                ++j;
+            }
+        }
+
+        return maxLength;
+    }
+};
